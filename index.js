278ad24246cb854bf4d03aebbc5bd77493c4edb5
Kanij Fatema <kfatema991@gmail.com>
2022-05-15 10:45:48 +0600

    set gitignore

--- index.js
+++ index.js
@@ -6,11 +6,9 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
+// middleware
 app.use(cors());
 app.use(express.json());
-
-// user:dbuser1
-// password:4Vq8Dz68YRa$p2Dv
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.3gzbo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -31,20 +29,35 @@ async function run() {
             const newArrivels = await cursor.toArray();
             res.send(newArrivels);
         });
-         app.get('/product', async (req, res) => {
+        app.get('/product', async (req, res) => {
             const query = {};
             const cursor = productCollection.find(query);
             const inventory = await cursor.toArray();
             res.send(inventory);
         });
-        
-        
+
+
         app.get('/product/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const product = await productCollection.findOne(query);
-            res.send(products);
-        })
+            res.send(product);
+        });
+
+        // POST
+        app.post('/product', async (req, res) => {
+            const newProduct = req.body;
+            const result = await productCollection.insertOne(newProduct);
+            res.send(result);
+        });
+
+        // DELETE
+        app.delete('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await productCollection.deleteOne(query);
+            res.send(result);
+        });
 
     }
     finally {
@@ -55,7 +68,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-    res.send('')
+    res.send('');
 });
 
 
